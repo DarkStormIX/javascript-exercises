6201a67b0ee9f9ed9e0dd458a20d719500b3d9cd
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-06 15:56:43 +0800

    Finish exercise 03
    reversed string using for loop, split and concat

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
+    const stringArr = string.split("");
 
+    for (let i = 0; i<stringArr.length; i++){
+        reversedString = reversedString.concat(stringArr[stringArr.length - i - 1]);
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
