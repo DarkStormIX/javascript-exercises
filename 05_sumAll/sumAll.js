8a15e666dfe6b95a12774f11c02129e7542ce0a3
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-07 20:27:14 +0800

    Finish exercise 05
    Sum all number between two given argument (inclusive)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    var sum = 0;
+    if(Number.isInteger(numOne) && Number.isInteger(numTwo) && 
+       numOne > 0 && numTwo > 0){
+        
+        if(numOne > numTwo){
+            sum = numTwo;
+            for(let i = 1; i<=(numOne - numTwo);i++){
+                sum += numTwo + i;
+            }
+        }
+        else{
+            sum = numOne;
+            for(let i = 1; i<=(numTwo - numOne);i++){
+                sum += numOne + i;
+            }
+        }
+        return sum;
+    }
+    else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
