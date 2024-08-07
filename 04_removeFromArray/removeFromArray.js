5044837cf89733d2ddba3f5388064769a45fd3b2
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-07 19:39:53 +0800

    Finish exercise 04
    Remove element from array that match the argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array = [],...theArgs) {
+    
+    for(const element of array){
+        for (const argument of theArgs){
+            if(element === argument){
+                array = array.filter((input) => input !== element);
+            }
+        }
+    }
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
