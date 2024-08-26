913e05b58a7e36a099908acd9a628f53e1ead99a
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-26 13:08:15 +0800

    Finish exercise 09
    Check for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let regex = /^[a-zA-Z0-9]+$/;
+    const array = string.split("");
+    const letters = array.filter(element => element.match(regex));
+    const finalArray = letters.map(element => element.toLowerCase());
+    
+    var counter = 0;
 
+    var length = 0;
+    if(finalArray.length % 2 == 0){
+        length = finalArray.length;
+    }
+    else{
+        length = finalArray.length -1;
+    }
+    for(let i = 0; i<length/2; i++){
+        if(finalArray[i] == finalArray[finalArray.length-1-i]){
+            counter++;
+        }
+    }
+
+    if(counter == length/2){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
