6cba334b2c910293c49df308ae0419486ea45e70
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-26 12:11:44 +0800

    FInish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	var sum = firstNumber + secondNumber;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	var difference = firstNumber - secondNumber;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(baseNumber, index) {
+	return Math.pow(baseNumber, index);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number == 0){
+    return 1;
+  }
+  else{
+    var sum = 1;
+    for(let i=1;i<=number;i++){
+      sum *= i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
