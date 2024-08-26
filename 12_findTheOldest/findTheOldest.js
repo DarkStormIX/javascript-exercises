5e317d9581bdd8339af2be7e13533c167b636657
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-26 14:43:59 +0800

    Finish exercise 12
    Create function to find object
    Face problem with object not having certain properties

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    
+    const age = array.map((element) => {
+        if(element.yearOfDeath == undefined){
+            element.yearOfDeath = 2024;
+        }
+        
+        return element.yearOfDeath - element.yearOfBirth;
 
+    });
+    var oldest = age.reduce((accumulator, currentValue) => {
+        if(accumulator>currentValue){
+            return accumulator
+        }
+        else{
+            return currentValue
+        }
+    })
+
+    var oldestIndex;
+    age.forEach((element, index) => {
+        if(element == oldest){
+            oldestIndex = index;
+        }
+    });
+
+    return array[oldestIndex];
 };
 
 // Do not edit below this line
