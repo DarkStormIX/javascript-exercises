7200aa919e983ebdc137794edde092c818317b4a
DarkStorm <150134437+DarkStormIX@users.noreply.github.com>
2024-08-26 13:20:06 +0800

    Finish exercise 10
    Return xth fibonacci members

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    const array = [1, 1];
+    number = Number(number);
+    if(number==0){
+        return 0;
+    }
+    else if(number>0){
+        for(let i = 1; i<number-1; i++){
+            array.push(array[i]+array[i-1]);
+        }
+        return array[number-1];
+    }
+    else{
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
